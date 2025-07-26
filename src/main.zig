c6d3635a557ac480f1c95e8a6400fd749f2d73bf
DanikVitek <x3665107@gmail.com>
2025-07-26 20:48:35 +0300

    `Vec3`

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const vec3 = @import("vec3.zig");
+const Vec3 = vec3.Vec3;
 
 pub fn main() !void {
     const stdout_file = std.io.getStdOut();
@@ -8,18 +10,30 @@ pub fn main() !void {
     const height = 256;
     const width = 256;
 
+    var pr_buf: [1024]u8 = undefined;
+    const progress = std.Progress.start(.{
+        .draw_buffer = &pr_buf,
+        .estimated_total_items = height * width,
+        .root_name = "drawing",
+    });
+    defer progress.end();
+
     try stdout.print("P3\n{d} {d}\n255\n", .{ width, height });
 
+    const fwidth: f32 = @floatFromInt(width);
+    const fheight: f32 = @floatFromInt(height);
     for (0..height) |y| {
         const fy: f32 = @floatFromInt(y);
         for (0..width) |x| {
             const fx: f32 = @floatFromInt(x);
 
-            const r: u8 = @intFromFloat(@trunc(255.99 * (fy / (height - 1.0))));
-            const g: u8 = @intFromFloat(@trunc(255.99 * (fx / (width - 1.0))));
-            const b: u8 = 0;
+            const color: Vec3 = .{
+                fx / (fwidth - 1),
+                fy / (fheight - 1),
+                0,
+            };
 
-            try stdout.print("{d: >3} {d: >3} {d: >3}\n", .{ r, g, b });
+            try stdout.print("{}\n", .{vec3.fmtColor(color)});
         }
     }
 
