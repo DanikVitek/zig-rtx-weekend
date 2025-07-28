764808ea355531cacf43622d8e456948cf8809c7
DanikVitek <x3665107@gmail.com>
2025-07-28 13:38:12 +0300

    Parallelize computations

--- src/camera.zig
+++ src/camera.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const Thread = std.Thread;
+const Progress = std.Progress;
+const Random = std.Random;
 
 const Vec3 = @import("Vec3.zig");
 const Ray = @import("Ray.zig");
@@ -8,8 +12,8 @@ const objects = @import("objects.zig");
 const Hit = objects.Hit;
 
 pub const aspect_ratio = 16.0 / 9.0;
-pub const img_width = 600;
-pub const samples_per_pixel = 30;
+pub const img_width = 1080;
+pub const samples_per_pixel = 100;
 
 const img_height = blk: {
     const fwidth: comptime_float = @as(comptime_float, img_width);
@@ -41,46 +45,120 @@ const pixel00_loc: Vec3 = .init(
 
 const max_recursion = 50;
 
-threadlocal var rand_state = std.Random.DefaultPrng.init(42);
-
-pub fn render(world: anytype) !void {
+pub fn render(world: anytype, allocator: Allocator, rand: Random) !void {
     const stdout_file = std.io.getStdOut();
     var stdout_buf = std.io.bufferedWriter(stdout_file.writer());
     const stdout = stdout_buf.writer();
 
     var pr_buf: [1024]u8 = undefined;
-    const progress = std.Progress.start(.{
+    const progress = Progress.start(.{
         .draw_buffer = &pr_buf,
-        .estimated_total_items = img_height * img_width,
+        .estimated_total_items = img_height * img_width * 3,
         .root_name = "drawing",
     });
     defer progress.end();
 
-    try stdout.print("P3\n{d} {d}\n255\n", .{ img_width, img_height });
+    const image = try allocator.alloc(Color, img_width * img_height);
+    defer allocator.free(image);
 
-    for (0..img_height) |y| {
-        for (0..img_width) |x| {
-            var pixel: Color = .black;
-            for (0..samples_per_pixel) |_| {
-                const ray: Ray = getRay(
-                    @floatFromInt(x),
-                    @floatFromInt(y),
-                );
-                pixel.v += rayColor(ray, world, 0).v;
-            }
-            pixel.v /= @splat(samples_per_pixel);
+    const grid = try factorizeParallelizm();
+    var threads: std.ArrayListUnmanaged(Thread) = try .initCapacity(
+        allocator,
+        std.math.mulWide(Sqrt(usize), grid.height, grid.width),
+    );
+    defer threads.deinit(allocator);
 
-            try stdout.print("{}", .{pixel});
-
-            progress.completeOne();
+    for (0..grid.height) |i| {
+        for (0..grid.width) |j| {
+            const kernel_x = j * (img_width / grid.width);
+            const kernel_y = i * (img_height / grid.height);
+            const kernel_width = @min(kernel_x + (img_width / grid.width), img_width) - kernel_x;
+            const kernel_height = @min(kernel_y + (img_height / grid.height), img_height) - kernel_y;
+            const thread = try Thread.spawn(
+                .{ .allocator = allocator },
+                kernel,
+                .{
+                    kernel_x,
+                    kernel_y,
+                    kernel_width,
+                    kernel_height,
+                    world,
+                    image,
+                    progress,
+                    rand,
+                },
+            );
+            threads.appendAssumeCapacity(thread);
         }
+    }
+
+    for (threads.items) |thread| {
+        thread.join();
+    }
+
+    try stdout.print("P3\n{d} {d}\n255\n", .{ img_width, img_height });
+    for (image) |pixel| {
+        try stdout.print("{}", .{pixel});
+        progress.completeOne();
     }
 
     try stdout_buf.flush();
 }
 
-fn getRay(x: f64, y: f64) Ray {
-    const offset = sampleSquare();
+fn factorizeParallelizm() !struct { width: Sqrt(usize), height: Sqrt(usize) } {
+    const parallelizm: usize = try Thread.getCpuCount();
+
+    const width: Sqrt(usize) = std.math.sqrt(parallelizm);
+    const height: Sqrt(usize) = @intCast(try std.math.divExact(usize, parallelizm, width));
+
+    return .{ .width = @max(width, height), .height = @min(width, height) };
+}
+
+fn Sqrt(comptime T: type) type {
+    return switch (@typeInfo(T)) {
+        .int => |info| std.meta.Int(.unsigned, (info.bits + 1) / 2),
+        else => T,
+    };
+}
+
+fn kernel(
+    kx: usize,
+    ky: usize,
+    kw: usize,
+    kh: usize,
+    world: anytype,
+    image: []Color,
+    progress: std.Progress.Node,
+    rand: Random,
+) void {
+    const fmt = "kernel({d}, {d})";
+    var name_buf: [std.fmt.count(fmt, .{std.math.maxInt(usize)} ** 2)]u8 = undefined;
+    const name: []const u8 = std.fmt.bufPrint(&name_buf, fmt, .{ kx, ky }) catch unreachable;
+
+    const kprogress = progress.start(name, kw * kh * samples_per_pixel);
+    defer kprogress.end();
+
+    for (kx..kx + kw) |x| {
+        for (ky..ky + kh) |y| {
+            var pixel: Color = .black;
+            for (0..samples_per_pixel) |_| {
+                const ray: Ray = getRay(
+                    rand,
+                    @floatFromInt(x),
+                    @floatFromInt(y),
+                );
+                pixel.v += rayColor(ray, rand, world, 0).v;
+
+                kprogress.completeOne();
+            }
+            pixel.v /= @splat(samples_per_pixel);
+            image[y * img_width + x] = pixel;
+        }
+    }
+}
+
+fn getRay(rand: Random, x: f64, y: f64) Ray {
+    const offset = sampleSquare(rand);
     const pixel_sample: Vec3 = .mulAdd(
         .splat(y + offset[1]),
         pixel_delta_v,
@@ -97,29 +175,28 @@ fn getRay(x: f64, y: f64) Ray {
     return .init(ray_origin, ray_dir);
 }
 
-fn sampleSquare() [2]f64 {
-    const rand = rand_state.random();
+fn sampleSquare(rand: Random) [2]f64 {
     const u = rand.float(f64);
     const v = rand.float(f64);
     return .{ u - 0.5, v - 0.5 };
 }
 
 fn rayColor(
-    r: Ray,
+    ray: Ray,
+    rand: Random,
     world: anytype,
     depth: std.math.IntFittingRange(0, max_recursion + 1),
 ) Color {
     if (depth > max_recursion) return .black;
 
-    if (hitWorld(world, r)) |hit| {
-        const rand = rand_state.random();
-        return if (hit.material.scatter(rand, r, hit)) |scatter|
-            scatter.attenuation.mul(rayColor(scatter.scattered, world, depth + 1))
+    if (hitWorld(world, ray)) |hit| {
+        return if (hit.material.scatter(rand, ray, hit)) |scatter|
+            scatter.attenuation.mul(rayColor(scatter.scattered, rand, world, depth + 1))
         else
             .black;
     }
 
-    const unit_direction = r.dir.normalized();
+    const unit_direction = ray.dir.normalized();
     const a: f64 = 0.5 * (unit_direction.y() + 1.0);
     const wat: Color = .init(.{ 0.5, 0.7, 1 });
     return .mulScalarAdd(a, wat, .splat(1.0 - a));
--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,18 @@ const Sphere = objects.Sphere;
 const camera = @import("camera.zig");
 
 pub fn main() !void {
+    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    // defer arena.deinit();
+    // var gpa: std.heap.ThreadSafeAllocator = .{ .child_allocator = arena.allocator() };
+    // const allocaror = gpa.allocator();
+    // const allocaror = arena.allocator();
+
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    defer if (gpa.deinit() != .ok) @panic("leak");
+    const allocator = gpa.allocator();
+    var rand_state = std.Random.DefaultPrng.init(42);
+    const rand = rand_state.random();
+
     const world = .{
         Sphere.init(
             .init(.{ 0, -100.5, -1 }),
@@ -30,5 +42,5 @@ pub fn main() !void {
             .{ .metal = .{ .albedo = .init(.{ 0.8, 0.6, 0.2 }) } },
         ),
     };
-    try camera.render(world);
+    try camera.render(world, allocator, rand);
 }
